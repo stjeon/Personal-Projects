15fba0c2de4d90af3592cac263f71dd212996610
Stephen Jeon <jeons99@gmail.com>
2017-12-18 20:26:29 -0500

    Added nav component

--- resume/src/app/app.module.ts
+++ resume/src/app/app.module.ts
@@ -5,13 +5,15 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { TimelineComponent } from './timeline/timeline.component';
+import { NavComponent } from './nav/nav.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    TimelineComponent
+    TimelineComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
